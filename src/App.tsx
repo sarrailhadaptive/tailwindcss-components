8996ebc12f4993d08ce9b978801fa454457b68f1
Matias Sarrailh <matias.sarrailh@weareadaptive.com>
2023-01-13 14:17:41 +0100

    Various Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+<<<<<<< HEAD
 import { btnBuyClass, btnSellClass } from "./assets/styles"
 import Button from "./components/Button"
+=======
+>>>>>>> e213bdc (Initial Commit + Menu Dropdown Component)
 import DropdownList from "./components/DropdownList"
 import MenuDropdown from "./components/MenuDropdown"
 import Modal from "./components/Modal"
@@ -11,6 +14,7 @@ function App() {
 
   return (
     <div className="min-h-screen bg-gray-200 text-gray-900">
+<<<<<<< HEAD
       <div className="container mx-auto grid grid-cols-4 gap-16">
         <div>
           <h2 className="font-bold text-xl pt-16">Menu (Dropdown)</h2>
@@ -58,6 +62,32 @@ function App() {
             <Button text="Forward" style={btnBuyClass} />
             <Button text="Back" style={btnBuyClass} />
           </div>
+=======
+      <div className="container mx-auto px-4">
+        <h2 className="font-bold text-xl pt-2">Menu (Dropdown)</h2>
+        <div className='mt-2'>
+          <MenuDropdown />
+        </div>
+        <h2 className="font-bold text-xl pt-16">Dialog (Modal)</h2>
+        <div className="mt-2">
+          <Modal />
+        </div>
+        <h2 className="font-bold text-xl pt-16">Radio Group</h2>
+        <div className="mt-2">
+          <RadioGroups />
+        </div>
+        <h2 className="font-bold text-xl pt-16">Tabs</h2>
+        <div className="mt-2">
+          <Tabs />
+        </div>
+        <h2 className="font-bold text-xl pt-16">Switch</h2>
+        <div className="mt-2">
+          <SwitchToggle />
+        </div>
+        <h2 className="font-bold text-xl pt-16">Disclosure</h2>
+        <div className="mt-2">
+          <DropdownList />
+>>>>>>> e213bdc (Initial Commit + Menu Dropdown Component)
         </div>
       </div >
     </div >
