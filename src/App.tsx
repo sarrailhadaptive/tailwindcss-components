bc522fa060c05e658e61191c172dee049fb1c962
Matias Sarrailh <matias.sarrailh@weareadaptive.com>
2023-01-13 14:17:41 +0100

    Initial Commit + Menu Dropdown Component

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import MenuDropdown from "./components/MenuDropdown"
+
+function App() {
+
+  return (
+    <div className="min-h-screen bg-gray-200 text-gray-900">
+      <div className="container mx-auto px-4">
+        <h2 className="font-bold">Menu (Dropdown)</h2>
+        <div className='mt-4'>
+          <MenuDropdown />
+        </div>
+      </div >
+    </div >
+  )
+}
+
+export default App
---
+++ src/components/MenuDropdown.tsx
@@ -0,0 +1,106 @@
+import { Menu, Transition } from '@headlessui/react'
+
+export default function MenuDropdown() {
+  return (
+    <>
+      <Menu>
+        <Menu.Button className="flex items-center bg-blue-500 text-white rounded-md px-4 py-2">
+          <span>More</span>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+          </svg>
+        </Menu.Button>
+        {/* I don't understand why the origin-top-left class in Menu.Items is not working, the menu is supposed to come from the top left
+            but instead is coming from the top right. I've tried moving it into different places and using other combinations but nothing is fixing the problem.
+            */}
+        <Transition
+          enter="transition duration-100 ease-out"
+          enterFrom="transform scale-75 opacity-0"
+          enterTo="transform scale-100 opacity-100"
+          leave="transition duration-100 ease-out"
+          leaveFrom="transform scale-100 opacity-100"
+          leaveTo="transform scale-75 opacity-0"
+        >
+          <Menu.Items className="absolute bg-white shadow-md rounded-lg flex flex-col focus:outline-none w-56 divide-y divide-gray-300 origin-top-left mt-2" as="ul">
+            <div className='px-1 py-1'>
+              <Menu.Item className="w-full" as="li">
+                {({ active }) => (
+                  <a
+                    className={`flex items-center rounded-md px-2 py-2 ${active && 'bg-blue-500 text-white'}`}
+                  >
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
+                    </svg>
+                    <span className='ml-2'>
+                      Edit
+                    </span>
+                  </a>
+                )}
+              </Menu.Item>
+              <Menu.Item className="" as="li">
+                {({ active }) => (
+                  <a
+                    className={`flex items-center rounded-md px-2 py-2 ${active && 'bg-blue-500 text-white'}`}
+                  >
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 01-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5a3.375 3.375 0 00-3.375-3.375H9.75" />
+                    </svg>
+                    <span className='ml-2'>
+                      Duplicate
+                    </span>
+                  </a>
+                )}
+              </Menu.Item>
+            </div>
+            <div className='px-1 py-1'>
+              <Menu.Item className="" as="li">
+                {({ active }) => (
+                  <a
+                    className={`flex items-center rounded-md px-2 py-2 ${active && 'bg-blue-500 text-white'}`}
+                  >
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z" />
+                    </svg>
+                    <span className='ml-2'>
+                      Archive
+                    </span>
+                  </a>
+                )}
+              </Menu.Item>
+              <Menu.Item className="" as="li">
+                {({ active }) => (
+                  <a
+                    className={`flex items-center rounded-md px-2 py-2 ${active && 'bg-blue-500 text-white'}`}
+                  >
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M10.05 4.575a1.575 1.575 0 10-3.15 0v3m3.15-3v-1.5a1.575 1.575 0 013.15 0v1.5m-3.15 0l.075 5.925m3.075.75V4.575m0 0a1.575 1.575 0 013.15 0V15M6.9 7.575a1.575 1.575 0 10-3.15 0v8.175a6.75 6.75 0 006.75 6.75h2.018a5.25 5.25 0 003.712-1.538l1.732-1.732a5.25 5.25 0 001.538-3.712l.003-2.024a.668.668 0 01.198-.471 1.575 1.575 0 10-2.228-2.228 3.818 3.818 0 00-1.12 2.687M6.9 7.575V12m6.27 4.318A4.49 4.49 0 0116.35 15m.002 0h-.002" />
+                    </svg>
+                    <span className='ml-2'>
+                      Move
+                    </span>
+                  </a>
+                )}
+              </Menu.Item>
+            </div>
+            <div className='px-1 py-1'>
+              <Menu.Item className="" as="li">
+                {({ active }) => (
+                  <a
+                    className={`flex items-center rounded-md px-2 py-2 ${active && 'bg-blue-500 text-white'}`}
+                  >
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                    </svg>
+                    <span className='ml-2'>
+                      Delete
+                    </span>
+                  </a>
+                )}
+              </Menu.Item>
+            </div>
+          </Menu.Items>
+        </Transition>
+      </Menu >
+    </>
+  )
+}
