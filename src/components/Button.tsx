3698c2c06c7b8195c6c6219d15e70bf41bdb6d29
Matias Sarrailh <matias.sarrailh@weareadaptive.com>
2023-01-17 15:20:33 +0100

    Added testing log

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,6 +6,7 @@ interface Button {
 }
 
 export default function Button({ text, style, disabled = "" }: Button) {
+  console.log("testing")
 
   return (
     <button className={style + disabled}>{text}</button>
